54584d3e772af5806408d57e505979289e1ac904
Ramin Keshavarzi <raminkeshavarzi@organiplace.com>
2021-03-14 23:00:41 +0100

    calculate BMI

--- Be Healthy/Be Healthy/ViewController.swift
+++ Be Healthy/Be Healthy/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var heightInput: UITextField!
+    @IBOutlet weak var weightInput: UITextField!
+    @IBOutlet weak var outPutLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        outPutLabel.text = "To check your BMI please add your data"
     }
 
+    @IBAction func bmiCalculator(_ sender: Any) {
+        let height = Double(heightInput.text!)!
+        let weight = Double(weightInput.text!)!
 
+//calculation
+        let bmi = Double(weight / (height * height))
+        let bodyMassIndex = Double(round(1000 * bmi)/1000)
+//rendering result
+        outPutLabel.text = "Your BMI is\(bodyMassIndex)"
+    }
+    
 }
 
